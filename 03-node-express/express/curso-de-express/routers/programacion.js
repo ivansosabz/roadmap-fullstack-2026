3afb24d04ebdee6ad10f01bb216d338df2704cab
ivansosabz <ivansosabz@gmail.com>
2026-04-02 18:12:40 -0300

    feat: get, put, patch and delete

--- 03-node-express/express/curso-de-express/routers/programacion.js
+++ 03-node-express/express/curso-de-express/routers/programacion.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { programacion } = require('../data/cursos.js').infoCursos;
 
 const routerProgramacion = express.Router();
+routerProgramacion.use(express.json()); // Middleware para parsear JSON
 routerProgramacion.get('/', (req, res) => {
     res.json(programacion);
 });
@@ -37,4 +38,42 @@ routerProgramacion.get('/:lenguaje', (req, res) => {
     }
 });
 
+routerProgramacion.post('/', (req, res) => {
+    let cursoNuevo = req.body; // Obtener el nuevo curso del cuerpo de la solicitud
+    programacion.push(cursoNuevo); // Agregar el nuevo curso al arreglo de programación
+    res.status(201).json(cursoNuevo); // Responder con el nuevo curso creado
+});
+routerProgramacion.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id); // Obtener el ID del curso a actualizar
+    const cursoIndex = programacion.findIndex(curso => curso.id === id); // Buscar el índice del curso en el arreglo
+    if (cursoIndex >=0 ) {
+        const cursoActualizado = { ...programacion[cursoIndex], ...req.body };// Crear un nuevo objeto de curso actualizado combinando el curso existente con los nuevos datos
+        programacion[cursoIndex] = cursoActualizado; // Actualizar el curso en el arreglo
+        res.json(cursoActualizado); // Responder con el curso actualizado
+    } else {
+        res.status(404).send(`No se encontró un curso de programación con el ID: ${id}`);
+    }
+});
+
+routerProgramacion.patch('/:id', (req, res) => { //pach se utiliza para actualizar parcialmente un recurso, es decir, solo los campos que se proporcionan en el cuerpo de la solicitud serán actualizados, mientras que los demás campos permanecerán sin cambios.
+    const id = parseInt(req.params.id); // Obtener el ID del curso a actualizar
+    const cursoIndex = programacion.findIndex(curso => curso.id === id); // Buscar el índice del curso en el arreglo
+    if (cursoIndex >=0 ) {
+        const cursoActualizado = { ...programacion[cursoIndex], ...req.body };// Crear un nuevo objeto de curso actualizado combinando el curso existente con los nuevos datos (solo los campos proporcionados en el cuerpo de la solicitud)
+        programacion[cursoIndex] = cursoActualizado; // Actualizar el curso en el arreglo
+        res.json(cursoActualizado); // Responder con el curso actualizado
+    } else {
+        res.status(404).send(`No se encontró un curso de programación con el ID: ${id}`);
+    }
+});
+routerProgramacion.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const cursoIndex = programacion.findIndex(curso => curso.id === id);
+    if (cursoIndex >= 0) {
+        programacion.splice(cursoIndex, 1);// Eliminar el curso del arreglo utilizando splice
+        res.json({ message: `Curso con ID: ${id} eliminado correctamente` });
+    } else {
+        res.status(404).send(`No se encontró un curso de programación con el ID: ${id}`);
+    }
+});
 module.exports = routerProgramacion;
