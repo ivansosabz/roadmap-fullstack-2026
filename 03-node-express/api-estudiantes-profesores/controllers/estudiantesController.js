e022c0e8a9fa97c63e2297babbdbc4c6bbab3ea4
ivansosabz <ivansosabz@gmail.com>
2026-04-06 15:34:44 -0300

    feat: read and insert 1:00

--- 03-node-express/api-estudiantes-profesores/controllers/estudiantesController.js
+++ 03-node-express/api-estudiantes-profesores/controllers/estudiantesController.js
@@ -3,14 +3,47 @@ class EstudiantesController {
     constructor () {
     }
     consultar (req, res) {
-        res.json({msg: "consulta de estudiantes"});
+        // const { dni, nombre, apellido, email } = req.body;
+        db.query(`select * from estudiantes`,
+            (err, result) => {
+                if (err) {
+                    return res.status(400).json(err);
+                }
+                return res.status(201).json({
+                    msg: "estudiantes",
+                    result
+                });
+            }
+        );
     }
     consultarDetalle (req, res) {
         const {id} = req.params;
-        res.json({msg: `consulta del estudiante ${id}`});
+
+        db.query(`select * from estudiantes where id = ?`, [id],
+            (err, result) => {
+                if (err) {
+                    return res.status(400).json(err);
+                }
+                return res.status(201).json({
+                    msg: `consulta del estudiante ${id}`,
+                    result
+                });
+            }
+        );
     }
-    ingresar (req, res) {
-        res.json({msg: "ingreso de estudiante"});
+    ingresar(req, res) {
+        const { dni, nombre, apellido, email } = req.body;
+        db.query(`INSERT INTO estudiantes (id, dni, nombre, apellido, email) VALUES (NULL, ?, ?, ?, ?)`, [dni, nombre, apellido, email],
+            (err, result) => {
+                if (err) {
+                    return res.status(400).json(err);
+                }
+                return res.status(201).json({
+                    msg: "Estudiante creado correctamente",
+                    result
+                });
+            }
+        );
     }
     actualizar (req, res) {
         const {id} = req.params;
